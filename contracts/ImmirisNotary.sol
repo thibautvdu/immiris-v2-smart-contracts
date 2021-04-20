2cded75e7e571f3d9d19bfa74593ddacb364ac2f
thibaut dumont <thibautdumont@pm.me>
2021-04-20 17:19:40 +0200

    remove event and add pc id string in certificate

--- contracts/ImmirisNotary.sol
+++ contracts/ImmirisNotary.sol
@@ -6,33 +6,40 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ImmirisNotary is Ownable {
-  event CreatedCertificate(string cPId, bytes32 hash);
+  //event CreatedCertificate(string constructionPermitId, bytes32 hash);
 
   struct Certificate {
+    string constructionPermitId;
     uint timestamp;
     uint blockNumber;
     address writerAddress;
   }
 
   mapping (bytes32 => Certificate) private certificates;
-  mapping (string => bytes32[]) private constuctionPermitToCertificates;
+  bytes32[] public certificateHashes;
 
-  function createCertificate(string calldata cPId, bytes32 hash) public onlyOwner {
+  mapping (string => bytes32[]) private constructionPermits;
+
+  function createCertificate(string calldata constructionPermitId, bytes32 hash) public onlyOwner {
       require(certificates[hash].writerAddress == address(0), "IMMIRIS: certificate already exists for this hash");
 
-      Certificate memory newCertificate = Certificate(block.timestamp, block.number, msg.sender);
-      certificates[hash] = newCertificate;
-      constuctionPermitToCertificates[cPId].push(hash);
+      certificates[hash].blockNumber = block.number;
+      certificates[hash].timestamp = block.timestamp;
+      certificates[hash].writerAddress = msg.sender;
+      certificates[hash].constructionPermitId = constructionPermitId;
 
-      emit CreatedCertificate(cPId, hash);
+      certificateHashes.push(hash);
+      constructionPermits[constructionPermitId].push(hash);
   }
 
   function getCertificate(bytes32 hash) public view returns (Certificate memory) {
     require(certificates[hash].writerAddress != address(0), "IMMIRIS: no certificate exists for this hash");
+
     return certificates[hash];
   }
 
-  function getHashesByConstructionPermit(string calldata cPId) public view returns (bytes32[] memory) {
-    return constuctionPermitToCertificates[cPId];
+
+  function getHashesByConstructionPermit(string calldata constructionPermitId) public view returns (bytes32[] memory) {
+    return constructionPermits[constructionPermitId];
   }
 }
--- test/ImmirisNotary.test.ts
+++ test/ImmirisNotary.test.ts
@@ -1,29 +1,29 @@
 import * as crypto from "crypto";
+import {expect} from 'chai';
 import '@nomiclabs/hardhat-ethers';
 import "@nomiclabs/hardhat-waffle";
 import {ethers, waffle} from 'hardhat';
-import {expect} from 'chai';
 import { Contract, ContractFactory } from '@ethersproject/contracts';
+
+// generate random construction permit ids
+var cPIds = [] as string[];
+const getRandomDigit = () => Math.floor(Math.random() * 10)
+const gerRandomLetter = () => String.fromCharCode(65+Math.floor(Math.random() * 26))
+for(var i = 0; i < 1000; i++) {
+  cPIds.push(`${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${gerRandomLetter()}`)
+}
+
+// generate random file hashes
+var fileHashes = [] as string[];
+const getRandomSHA256 = () => crypto.randomBytes(32).toString('hex');
+for(var i = 0; i < 1000; i++) {
+  fileHashes.push("0x" + getRandomSHA256());
+}
 
 // Start test block
 describe('ImmirisNotary', () => {
   let ImmirisNotary: ContractFactory;
   let notary: Contract;
-
-  // generate random pc ids
-  var pcIds = [] as string[];
-  const getRandomDigit = () => Math.floor(Math.random() * 10)
-  const gerRandomLetter = () => String.fromCharCode(65+Math.floor(Math.random() * 26))
-  for(var i = 0; i < 1000; i++) {
-    pcIds.push(`PC${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${getRandomDigit()}${gerRandomLetter()}`)
-  }
-
-  // generate random file hashes
-  var fileHashes = [] as string[];
-  const getRandomSHA256 = () => crypto.randomBytes(32).toString('hex');
-  for(var i = 0; i < 1000; i++) {
-    fileHashes.push("0x" + getRandomSHA256());
-  }
 
   before(async () => {
     ImmirisNotary = await ethers.getContractFactory("ImmirisNotary");
@@ -35,13 +35,13 @@ describe('ImmirisNotary', () => {
   });
 
   describe('Ownership', () => {
-    it('deployer is owner', async () => {
-      const [owner, other] = await ethers.getSigners();
+    it('Deployer should be owner', async () => {
+      const [owner] = await ethers.getSigners();
 
       expect(await notary.owner()).to.equal(owner.address);
     });
 
-    it('owner can transfer ownership', async () => {
+    it('Owner can transfer ownership', async () => {
       const [owner, other] = await ethers.getSigners();
 
       await notary.transferOwnership(other.address);
@@ -49,7 +49,7 @@ describe('ImmirisNotary', () => {
       expect(await notary.owner()).to.equal(other.address);
     });
 
-    it('non-owner can\'t transfer ownership', async () => {
+    it('Others can\'t transfer ownership', async () => {
       const [owner, other] = await ethers.getSigners();
 
       await expect(
@@ -59,13 +59,13 @@ describe('ImmirisNotary', () => {
       expect(await notary.owner()).to.equal(owner.address);
     });
 
-    it('anyone can see who is the owner', async () => {
+    it('Others can get the owner address', async () => {
       const [owner, other] = await ethers.getSigners();
 
       expect(await notary.connect(other).owner()).to.equal(owner.address);
     });
 
-    it('ownership transfer emits an event', async () => {
+    it('Ownership transfer should emit an event', async () => {
       const [owner, other] = await ethers.getSigners();
 
       await expect(
@@ -77,86 +77,153 @@ describe('ImmirisNotary', () => {
   });
 
   describe('Writing certificates', () => {
-    it('owner can create new certificates', async () => {
+    it('Owner can create new certificates', async () => {
       for(var i = 0; i < 10; i++) {
-        await expect(notary.createCertificate(pcIds[i],fileHashes[i])).to.not.be.reverted;
+        await expect(notary.createCertificate(cPIds[i],fileHashes[i])).to.not.be.reverted;
       }
     });
 
-    it('emit event on new certificate', async () => {
-      await expect(notary.createCertificate(pcIds[0],fileHashes[0])).to.emit(notary, "CreatedCertificate").withArgs(pcIds[0], fileHashes[0]);
-    });
+    // it('Creating a new certiicate should emit an event with the cp id and hash', async () => {
+    //   await expect(notary.createCertificate(cPIds[0],fileHashes[0])).to.emit(notary, "CreatedCertificate").withArgs(cPIds[0], fileHashes[0]);
+    // });
 
-    it('owner can create new certificates after ownership transfer', async () => {
+    it('Owner can create new certificates after ownership transfer', async () => {
       const [owner, other] = await ethers.getSigners();
 
       await notary.transferOwnership(other.address);
 
-      await expect(notary.connect(other).createCertificate(pcIds[0],fileHashes[0])).to.not.be.reverted;
+      await expect(notary.connect(other).createCertificate(cPIds[0],fileHashes[0])).to.not.be.reverted;
     });
 
-    it('non-owner can\'t create a new certificate', async () => {
+    it('Others can\'t create a new certificate', async () => {
       const [owner, other] = await ethers.getSigners();
 
-      await expect(notary.connect(other).createCertificate(pcIds[0],fileHashes[0])).to.be.reverted;
+      await expect(notary.connect(other).createCertificate(cPIds[0],fileHashes[0])).to.be.reverted;
     });
 
-    it('creating a new certificate with a prev. recorded file hash must revert', async () => {
-      await notary.createCertificate(pcIds[0],fileHashes[0]);
+    it('Creating a new certificate with a previously recorded file hash should fail', async () => {
+      await notary.createCertificate(cPIds[0],fileHashes[0]);
 
-      await expect(notary.createCertificate(pcIds[0],fileHashes[0])).to.be.revertedWith("IMMIRIS: certificate already exists for this hash");
-      await expect(notary.createCertificate(pcIds[1],fileHashes[0])).to.be.revertedWith("IMMIRIS: certificate already exists for this hash");
+      await expect(notary.createCertificate(cPIds[0],fileHashes[0])).to.be.revertedWith("IMMIRIS: certificate already exists for this hash");
+      await expect(notary.createCertificate(cPIds[1],fileHashes[0])).to.be.revertedWith("IMMIRIS: certificate already exists for this hash");
     });
 
-    it('owner can create new certificate with the same prev. recorded PC ID but new hashes', async () => {
+    it('Owner can create new certificate with a previously recorded cp id and a new hash', async () => {
       for(var i = 0; i < 10; i++) {
-        await expect(notary.createCertificate(pcIds[0],fileHashes[i])).to.not.be.reverted;
+        await expect(notary.createCertificate(cPIds[0],fileHashes[i])).to.not.be.reverted;
       }
     });
+
+    // it('Certificate gas creation should be constant', async () => {
+    //   var tx = await notary.createCertificate(cPIds[0],fileHashes[0]);
+    //   var gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //
+    //   tx = await notary.createCertificate(cPIds[1],fileHashes[1]);
+    //   gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //   console.log("first hash gas",gas.toNumber());
+    //
+    //
+    //   tx = await notary.createCertificate(cPIds[1],fileHashes[2]);
+    //   gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //   console.log("second hash gas",gas.toNumber());
+    //
+    //   tx = await notary.createCertificate(cPIds[1],fileHashes[3]);
+    //   gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //   console.log("third hash gas",gas.toNumber());
+    //
+    //   tx = await notary.createCertificate(cPIds[2],fileHashes[4]);
+    //   gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //   console.log("second pc gas",gas.toNumber());
+    //
+    //   tx = await notary.createCertificate(cPIds[3],fileHashes[5]);
+    //   gas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed;
+    //   console.log("third pc gas",gas.toNumber());
+    //
+    //   // for(var i = 2; i < 1000; i++) {
+    //   //   tx = await notary.createCertificate(cPIds[i%10],fileHashes[i]);
+    //   //   let txGas = (await waffle.provider.getTransactionReceipt(tx.hash)).gasUsed
+    //   //   expect(txGas.toNumber()).to.be.closeTo(gas.toNumber(),20000);
+    //   // }
+    // });
   });
 
   describe('Reading certificates', () => {
-    it('anyone can read a certificate data given a file hash', async () => {
+    it('Others can read a certificate data given a file hash', async () => {
       const [owner, other] = await ethers.getSigners();
 
-      await notary.createCertificate(pcIds[0],fileHashes[0]);
+      await notary.createCertificate(cPIds[0],fileHashes[0]);
 
       await expect(notary.connect(other).getCertificate(fileHashes[0])).to.not.be.reverted;
     });
 
-    it('return the previously stored certificate data given a file hash', async () => {
+    it('Certificate\'s block number matches tranasaction\'s block number', async () => {
       const [owner, other] = await ethers.getSigners();
 
       for(var i = 0; i < 10; i++) {
-        var creationTx = await notary.createCertificate(pcIds[i],fileHashes[i]);
+        var creationTx = await notary.createCertificate(cPIds[i],fileHashes[i]);
         var certificate = await notary.connect(other).getCertificate(fileHashes[i]);
 
         expect(certificate.blockNumber).to.equal(creationTx.blockNumber);
-        expect((await waffle.provider.getBlock(certificate.blockNumber.toHexString())).timestamp).to.equal(certificate.timestamp);
-        expect(certificate.writerAddress).to.equal(owner.address);
       }
     });
 
-    it('return the files hashes associated with the input PC ID', async () => {
+    it('Certificate\'s timestamp matches tranasaction\'s block\'s timestamp', async () => {
+      const [owner, other] = await ethers.getSigners();
+
+      for(var i = 0; i < 10; i++) {
+        var creationTx = await notary.createCertificate(cPIds[i],fileHashes[i]);
+        var certificate = await notary.connect(other).getCertificate(fileHashes[i]);
+
+        expect((await waffle.provider.getBlock(certificate.blockNumber.toHexString())).timestamp).to.equal(certificate.timestamp);
+      }
+    });
+
+    it('Certificate\'s writer address matches contract owner address', async () => {
+      const [owner, other] = await ethers.getSigners();
+
+      await notary.createCertificate(cPIds[0],fileHashes[0]);
+      var certificate = await notary.connect(other).getCertificate(fileHashes[0]);
+
+      expect(certificate.writerAddress).to.equal(owner.address);
+
+      await notary.transferOwnership(other.address);
+
+      await notary.connect(other).createCertificate(cPIds[1],fileHashes[1]);
+      var certificate2 = await notary.getCertificate(fileHashes[1]);
+
+      expect(certificate2.writerAddress).to.equal(other.address);
+    });
+
+    it('Certificate\'s cp id matches previous input', async () => {
+      const [owner, other] = await ethers.getSigners();
+
+      await notary.createCertificate(cPIds[0],fileHashes[0]);
+      var certificate = await notary.connect(other).getCertificate(fileHashes[0]);
+
+      expect(certificate.constructionPermitId).to.equal(cPIds[0]);
+    });
+
+    it('Others can retrieve the associated certificate\'s file hashes given a cp id', async () => {
       var hashesTofind = [];
 
-      for(var i = 0; i < 50; i++) {
-        await notary.createCertificate(pcIds[i],fileHashes[i]);
+      for(var i = 0; i < 10; i++) {
+        await notary.createCertificate(cPIds[i],fileHashes[i]);
       }
-      for(var i = 500; i < 510; i++) {
-        await notary.createCertificate(pcIds[500],fileHashes[i]);
+      for(var i = 100; i < 110; i++) {
+        await notary.createCertificate(cPIds[100],fileHashes[i]);
         hashesTofind.push(fileHashes[i]);
       }
-      for(var i = 510; i < 550; i++) {
-        await notary.createCertificate(pcIds[i],fileHashes[i]);
+      for(var i = 510; i < 520; i++) {
+        await notary.createCertificate(cPIds[i],fileHashes[i]);
       }
 
-      var hashes = await notary.getHashesByConstructionPermit(pcIds[500]);
-      expect(hashesTofind.every(h => hashes.includes(h)) && hashes.every(h => hashesTofind.includes(h)));
+      var hashes = await notary.getHashesByConstructionPermit(cPIds[100]);
+      expect(hashesTofind.every(h => hashes.includes(h))).to.be.true;
+      expect(hashes.every(h => hashesTofind.includes(h))).to.be.true;
     });
 
-    it('requesting a certificate with a non-recorded hash should revert', async () => {
-        await notary.createCertificate(pcIds[0],fileHashes[0]);
+    it('Requesting a certificate with a non-recorded hash should fail', async () => {
+        await notary.createCertificate(cPIds[0],fileHashes[0]);
         await expect(notary.getCertificate(fileHashes[1])).to.be.revertedWith("IMMIRIS: no certificate exists for this hash");
     });
   });
